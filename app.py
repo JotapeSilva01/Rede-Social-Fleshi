c89bf1aa9a1350b74fe04b3a76142f517a77fe80
João <joaopedrodasilva874@gmail.com>
2025-11-12 14:12:28 -0300

    chore: initial  project configuration

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bem-vindo ao Fléshi!'
+    return 'Bem-vindo ao Fsdsdsdsdléshi!'
 
 if __name__ == '__main__':
     app.run(debug=True)
