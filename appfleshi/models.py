dc460d77326c623ce3468715ead48f4600094849
João <joaopedrodasilva876@gmail.com>
2025-11-19 14:57:13 -0300

    feat: login and security system setup

--- appfleshi/models.py
+++ appfleshi/models.py
@@ -1,7 +1,21 @@
-from appfleshi import database
+from appfleshi import database, login_manager
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from flask_login import UserMixin
 
-class User(database.Model):
-    pass
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(database.Model, UserMixin):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20), unique=True, nullable=False)
+    email = database.Column(database.String(100), unique=True, nullable=False)
+    password = database.Column(database.String(60), nullable=False)
+    photos = database.relationship('Photo', backref='user', lazy=True)
 
 class Photo(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+    file_name = database.Column(database.String(255), nullable=False, default='default.png')
+    upload_date = database.Column(database.DateTime, nullable=False, default=lambda:datetime.now(ZoneInfo('America/Sao_Paulo')))
--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,10 +1,15 @@
 from flask import render_template
+from flask_login import login_required
+
 from appfleshi import app
 
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
 
+
 @app.route("/profile/<username>")
+@login_required
 def perfil(username):
+
     return render_template('profile.html', username=username)
